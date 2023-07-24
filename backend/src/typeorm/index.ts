dda099befc56148301a3dd98633ff60509d6177b
AntoineA67 <adamy.antoine@gmail.com>
2023-07-24 16:03:17 +0200

    changed folder architecture

--- backend/src/typeorm/index.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "./user.entity";
-import Message from "./message.entity";
-
-const entities = [User, Message,];
-
-export { User, Message, };
-export default entities;
---
+++ src/backend/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:8000',
+    ],
+    methods: ["GET", "POST"],
+  });
+  await app.listen(3000);
+}
+bootstrap();
