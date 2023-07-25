9880fe90cc0f42595de776a91e25ac90c53b76b1
Antoine Adamy <antoine.adamy@gmail.com>
2023-07-25 19:27:51 +0200

    ping pong

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -6,6 +6,8 @@ import {
 } from "react-router-dom";
 import Home from './Home';
 import Game from './Game';
+import Intro from './ping-pong/Intro';
+import PingPong from './ping-pong/PingPong';
 
 axios.defaults.baseURL = 'http://127.0.0.1:3000';
 
@@ -21,7 +23,9 @@ let router = createBrowserRouter([
     path: "/game",
     loader: () => ({ message: "Hello Data Router!" }),
     Component() {
-      return Game();
+      return <><Intro>
+        <PingPong ready={true} />
+      </Intro></>;
     },
   },
 ]);
