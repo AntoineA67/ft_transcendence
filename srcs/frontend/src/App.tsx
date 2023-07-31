eef508072aa6a7d2cf418145d0ab3e06d5e64981
Antoine Adamy <antoine.adamy@gmail.com>
2023-07-31 15:43:00 +0200

    first move

--- srcs/frontend/src/App.tsx
+++ srcs/frontend/src/App.tsx
@@ -6,28 +6,36 @@ import {
 } from "react-router-dom";
 import Home from './Home';
 import Game from './Game';
-import Intro from './ping-pong/Intro';
-import PingPong from './ping-pong/PingPong';
+import Menu from './Menu';
+// import Intro from './ping-pong/Intro';
+// import PingPong from './ping-pong/PingPong';
 
 axios.defaults.baseURL = 'http://127.0.0.1:3000';
 
 let router = createBrowserRouter([
   {
-    path: "/",
+    path: "/home",
     loader: () => ({ message: "Hello Data Router!" }),
     Component() {
       return Home();
     },
   },
   {
-    path: "/game",
+    path: "/",
     loader: () => ({ message: "Hello Data Router!" }),
     Component() {
-      return <><Intro>
-        <PingPong ready={true} />
-      </Intro></>;
+      return Menu();
     },
   },
+  // {
+  //   path: "/game",
+  //   loader: () => ({ message: "Hello Data Router!" }),
+  //   Component() {
+  //     return <><Intro>
+  //       <PingPong ready={true} />
+  //     </Intro></>;
+  //   },
+  // },
 ]);
 
 export default function App() {
