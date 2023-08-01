7294149c66851904f3c67de5f50c123c9398261e
Antoine Adamy <antoine.adamy@gmail.com>
2023-08-01 14:57:52 +0200

    changed backend folder structure

--- srcs/backend/src/app.module.ts
+++ srcs/backend/src/app.module.ts
@@ -1,36 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import entities from './typeorm';
 import { MessagesModule } from './message/message.module';
 import { MessagesController } from './message/messages.controller';
 import { MessagesService } from './message/messages.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostgresProviderModule } from './providers/db/provider.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        // host: 'localhost',
-        host: configService.get('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        // port: 5432,
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: entities,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
     UsersModule,
     MessagesModule,
+    PostgresProviderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
