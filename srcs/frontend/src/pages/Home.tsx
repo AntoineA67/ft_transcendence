d701142703f84e3aa2fda9c133bbfe768620abcb
Antoine Adamy <antoine.adamy@gmail.com>
2023-08-01 17:06:31 +0200

    minor fixes in frontend + env backend

--- srcs/frontend/src/pages/Home.tsx
+++ srcs/frontend/src/pages/Home.tsx
@@ -9,29 +9,37 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 import { LinkContainer } from 'react-router-bootstrap'
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function BasicExample() {
+	return (
+		<Card style={{ width: '18rem' }}>
+			<Card.Img variant="top" src="holder.js/100px180" />
+			<Card.Body>
+				<Card.Title>Card Title</Card.Title>
+				<Card.Text>
+					Some quick example text to build on the card title and make up the
+					bulk of the card's content.
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	);
+}
+
 
 function Home() {
 	return (
 		<Container>
-			<Stack direction="horizontal" gap={2}>
-				<LinkContainer to="/game">
-					<Button as="a" variant="primary">
-						Game
-					</Button>
-				</LinkContainer>
-				<LinkContainer to="/profile">
-					<Button as="a" variant="primary">
-						Profile
-					</Button>
-				</LinkContainer>
-				<LinkContainer to="/test-db">
-					<Button as="a" variant="primary">
-						Test DB
-					</Button>
-				</LinkContainer>
-			</Stack>
-		</Container>
+			<BasicExample />
+			<Row>
+				<Button color='primary'> Primary </Button>
+
+			</Row>
+
+		</Container >
 	);
+
 }
 
 export default Home;
