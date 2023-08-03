6912ddd65f2e4b9d2bc362de104368e5a8d0763c
Antoine Adamy <antoine.adamy@gmail.com>
2023-08-03 14:04:45 +0200

    fix root route on backend not public

--- srcs/backend/src/app.controller.ts
+++ srcs/backend/src/app.controller.ts
@@ -1,15 +1,31 @@
 
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './auth/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) { }
 
+  @Public()
+  @Get('/')
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  // Must be public before localAuthGuard to allow login
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- srcs/backend/src/auth/auth.module.ts
+++ srcs/backend/src/auth/auth.module.ts
@@ -2,6 +2,7 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
@@ -16,7 +17,7 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule { }
