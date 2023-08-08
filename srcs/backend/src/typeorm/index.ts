467f4828e535f85d2fdf0ae46a8c504d915594dc
Antoine Adamy <antoine.adamy@gmail.com>
2023-08-08 11:48:46 +0200

    fix no metadata for Game was found, GRAPHQL OK !

--- srcs/backend/src/typeorm/index.ts
+++ srcs/backend/src/typeorm/index.ts
@@ -2,7 +2,7 @@ import { User } from "./user.entity";
 import Message from "./message.entity";
 import { Game } from "./game.entity";
 
-const entities = [User, Message,];
+const entities = [User, Message, Game];
 
 export { User, Message, Game };
 export default entities;
