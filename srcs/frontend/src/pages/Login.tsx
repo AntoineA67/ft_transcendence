4a0529974628c3bf06abb7790d0ce04520b58bcf
Hsin Yu Liu <dewseileaf@gmail.com>
2023-08-10 22:06:03 +0200

    landing page mostly ok

--- srcs/frontend/src/pages/Login.tsx
+++ srcs/frontend/src/pages/Login.tsx
@@ -2,6 +2,8 @@ import '../styles/Login.css'
 import '../styles/customButton.css'
 import bg from '../assets/landingBg.png';
 import githubLogo from '../assets/github.svg';
+import { useState } from 'react';
+import LeftArrow from '../utils/LeftArrow';
 
 import Stack from 'react-bootstrap/Stack';
 import Container from 'react-bootstrap/Container';
@@ -9,24 +11,78 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Login() {
+	const [page, setPage] = useState<string>('landing');
+	
+	const gotoSignin = () => {
+		setPage('signin');
+	}
+	const gotoSignup = () => {
+		setPage('signup');
+	}
+	// const goto42 = () => {
+	// 	setPage('')
+	// }
+	const goToLanding = () => {
+		setPage('landing');
+	}
+	
 	return (
-		<LandingPage></LandingPage>
+		<body>
+			{page == 'landing' && <LandingPage 
+				click1={gotoSignin}
+				click2={gotoSignup}
+				// click3={handleClick42}
+				/>}
+			{page === 'signin' && <Signin
+				setPage={setPage}
+			></Signin>}
+			{page === 'signup' && <Signup
+				setPage={setPage}
+			></Signup>}
+		</body>
 	);
 }
 
-
-function Signup() {
-
+type signupProps = {
+	setPage: React.Dispatch<React.SetStateAction<string>>,
 }
 
-function Signin() {
-
-}
-
-function LandingPage() {
+function Signup({setPage}: signupProps) {
 	return (
-		<div className="landingPage"
-			style={{ backgroundImage: `url(${bg})` }}>
+		<div>
+			<LeftArrow
+				setPage={setPage}
+				goToPage={'landing'}
+			></LeftArrow>
+			<h1>signup</h1>
+		</div>
+	)
+}
+
+type signinProps = {
+	setPage: React.Dispatch<React.SetStateAction<string>>,
+}
+
+function Signin({ setPage }: signinProps) {
+	return (
+		<div>
+			<LeftArrow
+				setPage={setPage}
+				goToPage={'landing'}
+			></LeftArrow>
+			<h1>signup</h1>
+		</div>
+	)
+}
+
+type landingPageProps = {
+	click1: () => void,
+	click2: () => void,
+}
+
+function LandingPage({click1, click2}: landingPageProps) {
+	return (
+		<div className="landingPage">
 			<div>
 				<h2>PingPangPong</h2>
 				<p style={{ color: "gray" }}>
@@ -36,9 +92,21 @@ function LandingPage() {
 				</p>
 			</div>
 			<div>
-				<button className="btn-magenta">Login</button><br />
-				<button className="btn-outline-magenta">Sign up</button><br/>
-				<button className="btn-invisible">Sign in with 42</button>
+				<button 
+					onClick={click1}
+					className="btn-magenta">
+					Login
+				</button>
+				<button
+					onClick={click2} 
+					className="btn-outline-magenta">
+					Sign up
+				</button>
+				<button 
+					// onClick={click3}
+					className="btn-invisible">
+					Sign in with 42
+				</button>
 			</div>
 			<footer className="footer">
 				Projet de fin de tronc-commun de l’école 42<br />
