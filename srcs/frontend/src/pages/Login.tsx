06a9558570dd5548188799e995884272d3f8fa52
Hsin Yu Liu <dewseileaf@gmail.com>
2023-08-12 16:35:57 +0200

    norm

--- srcs/frontend/src/pages/Login.tsx
+++ srcs/frontend/src/pages/Login.tsx
@@ -1,64 +1,21 @@
 import '../styles/index.css'
 import '../styles/Login.css'
+// import '../styles/customButton.css'
+
+import githubLogo from '../assets/github.svg';
 import eyeopen from '../assets/eyeopen.svg';
 import eyeclose from '../assets/eyeclose.svg';
-// import '../styles/customButton.css'
-import bg from '../assets/landingBg.png';
-import githubLogo from '../assets/github.svg';
+
 import { useState } from 'react';
-import LeftArrow from '../utils/LeftArrow';
+
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-
-
-import Stack from 'react-bootstrap/Stack';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-
-
 function Login() {
 	const [page, setPage] = useState<string>('landing');
-	
-	const gotoSignin = () => {
-		setPage('signin');
-	}
-	const gotoSignup = () => {
-		setPage('signup');
-	}
-	// const goto42 = () => {
-	// 	setPage('')
-	// }
-	const goToLanding = () => {
-		setPage('landing');
-	}
-	
-	return (
-		<body>
-			{page == 'landing' && <LandingPage 
-				click1={gotoSignin}
-				click2={gotoSignup}
-				// click3={handleClick42}
-				/>}
-			{page === 'signin' && <Signin
-				setPage={setPage}
-			></Signin>}
-			{page === 'signup' && <Signup
-				setPage={setPage}
-			></Signup>}
-		</body>
-	);
-}
 
-/* sign up page */
-
-
-type signupProps = {
-	setPage: React.Dispatch<React.SetStateAction<string>>,
-}
-
-function Signup({setPage}: signupProps) {
 	const togglePassword = () => {
 		let x = document.getElementById("password") as HTMLInputElement;
 		let eye = document.getElementById("eye") as HTMLImageElement;
@@ -72,13 +29,36 @@ function Signup({setPage}: signupProps) {
 	}
 	
 	return (
+		<body>
+			{page == 'landing' && <LandingPage 
+				handleSignin={() => setPage('signin')}
+				handleSignup={() => setPage('signup')} />}
+			{page === 'signin' && <Signin 
+				handleLanding={() => setPage('landing')}
+				togglePassword={togglePassword} />}
+			{page === 'signup' && <Signup 
+				handleLanding={() => setPage('landing')}
+				togglePassword={togglePassword} />}
+		</body>
+	);
+}
+
+/* sign up page */
+
+type signupProps = {
+	handleLanding: () => void,
+	togglePassword: () => void,
+}
+
+function Signup({ handleLanding, togglePassword }: signupProps) {	
+	return (
 		<Container>
 			<Row className="justify-content-center">
 				<Col sm="6" lg="4" >
+					<button 
+						className="leftArrow my-4"
+						onClick={handleLanding} />
 					<Form className="w-100">
-						<div className="my-4">
-							<LeftArrow setPage={setPage} goToPage={'landing'} />
-						</div>
 						<h3 style={{ color: "white" }}>New Account!</h3>
 						
 						<Form.Group className="my-4" controlId="nickname">
@@ -126,31 +106,19 @@ function Signup({setPage}: signupProps) {
 /* sign in page */
 
 type signinProps = {
-	setPage: React.Dispatch<React.SetStateAction<string>>,
+	handleLanding: () => void,
+	togglePassword: () => void,
 }
 
-function Signin({ setPage }: signinProps) {
-	
-	const togglePassword = () => {
-		let x = document.getElementById("password") as HTMLInputElement;
-		let eye = document.getElementById("eye") as HTMLImageElement;
-		if (x.type === "password") {
-			x.type = "text";
-			eye.src = eyeclose;
-		} else {
-			x.type = "password";
-			eye.src = eyeopen;
-		}
-	}
-
+function Signin({ handleLanding, togglePassword }: signinProps) {
 	return (
 		<Container>
 			<Row className="justify-content-center">
 				<Col sm="6" lg="4" >
+					<button
+						className="leftArrow my-4"
+						onClick={handleLanding} />
 					<Form className="w-100">
-						<div className="my-4">
-							<LeftArrow setPage={setPage} goToPage={'landing'} />
-						</div>
 						<h3 style={{ color: "white" }}>Welcome back!</h3>
 
 						<Form.Group className="my-4" controlId="nickname">
@@ -196,11 +164,11 @@ function Signin({ setPage }: signinProps) {
 /* landing page */
 
 type landingPageProps = {
-	click1: () => void,
-	click2: () => void,
+	handleSignin: () => void,
+	handleSignup: () => void,
 }
 
-function LandingPage({click1, click2}: landingPageProps) {
+function LandingPage({ handleSignin, handleSignup }: landingPageProps) {
 	
 	return (
 		<>
@@ -220,12 +188,12 @@ function LandingPage({click1, click2}: landingPageProps) {
 							justify-content-center align-items-center "
 							style={{position: "relative", top: "30px"}}>
 							<button
-								onClick={click1}
+								onClick={handleSignin}
 								className="btn btn-primary w-75">
 								Login
 							</button>
 							<button
-								onClick={click2}
+								onClick={handleSignup}
 								className="btn btn-outline-primary w-75">
 								Sign up
 							</button>
