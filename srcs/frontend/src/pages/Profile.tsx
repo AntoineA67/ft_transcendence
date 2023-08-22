f2a6745997700d620a37da35814150f07006a7f8
Hsin Yu Liu <dewseileaf@gmail.com>
2023-08-22 23:08:51 +0200

    avatar sketch

--- srcs/frontend/src/pages/Profile.tsx
+++ srcs/frontend/src/pages/Profile.tsx
@@ -4,6 +4,7 @@ import '../styles/Profile.css';
 import { useState } from 'react';
 import Container from 'react-bootstrap/Container'; 
 import EditPen from '../assets/EditPen.svg';
+import Setting from '../assets/setting.svg';
 
 type achievement = {
 	date: string,
@@ -43,15 +44,21 @@ function Profile() {
 	//get user data with fetch
 	
 	return (
-		<Container className="py-5 text-center" style={{color: "white", border: "2px solid white"}}>
-			<h5 className="py-5" >
+		<Container className="my-5 text-center" style={{color: "white", border: "2px solid white"}}>
+			<div className="position-absolute top-0 end-0">
+				<img src={Setting} className="m-3 pale-little-icon" />
+			</div>
+			<img alt="avatar" src={EditPen}
+				style={{height: "200px", width: "200px", borderRadius: "50%", border: "1px solid white"}} />
+			
+			<h5 className="my-5" >
 				Intelligent Seagul
-				<span><img src={EditPen} className="edit-pen" /></span>
+				<span><img src={EditPen} className="pale-little-icon" /></span>
 			</h5>
 			<p className="mx-5 text-break" >
 				I live by the sea, but far from the bay. I play pingpong mid flight. I rob people of their chips.
 				Gaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa~
-				<span><img src={EditPen} className="edit-pen" /></span>
+				<span><img src={EditPen} className="pale-little-icon" /></span>
 			</p>
 
 		</Container>
