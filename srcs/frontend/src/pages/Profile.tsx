7587300381c06858b89c576ffa45adb0781c7ed2
Hsin Yu Liu <dewseileaf@gmail.com>
2023-08-24 12:08:00 +0200

    edit profile

--- srcs/frontend/src/pages/Profile.tsx
+++ srcs/frontend/src/pages/Profile.tsx
@@ -7,13 +7,12 @@ import { Link } from "react-router-dom";
 import { useUser } from './Sidebar';
 
 import Container from 'react-bootstrap/Container'; 
-import EditPen from '../assets/EditPen.svg';
-import Setting from '../assets/setting.svg';
+
 
 type textProp = {
 	type: 'nick' | 'bio',
 	content: string,
-	setEdit: React.Dispatch<React.SetStateAction<"bio" | "no" | "nick">>,
+	setEdit: React.Dispatch<React.SetStateAction<"bio" | "done" | "nick">>,
 }
 
 function Text({type, content, setEdit}: textProp) {
@@ -31,14 +30,12 @@ type editTextProp = {
 	type: 'nick' | 'bio',
 	content: string,
 	setContent: React.Dispatch<React.SetStateAction<string>>,
-	setFinal: React.Dispatch<React.SetStateAction<string>>,
-	setEdit: React.Dispatch<React.SetStateAction<"bio" | "no" | "nick">>,
+	setEdit: React.Dispatch<React.SetStateAction<"bio" | "done" | "nick">>,
 }
 
-function EditText({type, content, setContent, setFinal, setEdit}: editTextProp) {
+function EditText({type, content, setContent, setEdit}: editTextProp) {
+	const [mod, setMod] = useState(content);
 	
-	
-
 	async function handleSubmit(e: React.FormEvent<HTMLFormElement>, type: string, content: string) {
 		e.preventDefault();
 		const fetchObj = {
@@ -55,16 +52,15 @@ function EditText({type, content, setContent, setFinal, setEdit}: editTextProp) 
 		} finally {
 			//if response ok, update
 			//if not do nothing
-			//for now simply update
-			setFinal(content);
-			setEdit('no');
+			setContent(mod);
+			setEdit('done');
 		}
 	}
 	
 	return (
 		<form onSubmit={(e) => handleSubmit(e, type, content)}>
-			<label htmlFor="editing">hi</label>
-			<input id="editing" value={content} onChange={(e) => setContent(e.target.value)}></input>
+			<label htmlFor={`edit-${type}`}></label>
+			<input id={`edit-${type}`} value={mod} onChange={(e) => setMod(e.target.value)}></input>
 			<button type="submit" className="ok"/>
 		</form>
 	);
@@ -73,11 +69,8 @@ function EditText({type, content, setContent, setFinal, setEdit}: editTextProp) 
 
 function Profile() {
 	const { nickname, bio, avatar, setNickname, setBio, setAvatar } = useUser();
-	const [text, setText] = useState('');
-	const [edit, setEdit] = useState<'no' | 'nick' | 'bio'>('no');
+	const [edit, setEdit] = useState<'done' | 'nick' | 'bio'>('done');
 
-	
-	
 	return (
 		<Container className="my-5 d-flex flex-column align-items-center" style={{color: "white", border: "2px solid white"}}>
 			<div className="position-absolute top-0 end-0">
@@ -88,14 +81,16 @@ function Profile() {
 				style={{height: "100px", width: "100px", borderRadius: "50%", border: "1px solid white"}} />
 			
 			<div>
-				<label></label>
-				<input id="nick" className="text-center input-profile" readOnly value={editnick} />
-				<button id="edit-nick" className="edit-pen" onClick={() => edit('nick')} />
+				{edit == 'nick' ?
+					 <EditText type={'nick'} content={nickname} setContent={setNickname} setEdit={setEdit}/>
+					: <Text type={'nick'} content={nickname} setEdit={setEdit} />} 
 			</div>
 
-
-			<input id="bio" className="text-center text-break" readOnly value={editbio} />			
-			<button id="edit-bio" className="ok" onClick={() => edit('bio')} />
+			<div>
+				{edit == 'bio' ?
+					<EditText type={'bio'} content={bio} setContent={setBio} setEdit={setEdit}/>
+					: <Text type={'bio'} content={bio} setEdit={setEdit} /> }
+			</div>
 
 		</Container>
 	);
