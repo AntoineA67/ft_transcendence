da6477affd2eecea79d9c112c97782538a48bf47
AlricPerso <alric.conte@gmail.com>
2023-08-25 20:07:59 +0200

    up

--- srcs/backend/src/users/users.module.ts
+++ srcs/backend/src/users/users.module.ts
@@ -1,9 +1,10 @@
-
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService],
   exports: [UsersService],
+  providers: [UsersService],
+  imports: [PrismaModule],
 })
-export class UsersModule { }
+export class UsersModule {}
