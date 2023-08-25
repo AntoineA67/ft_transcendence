061679b0d30bd5e1dfc612d0b005111443d042a1
Antoine Adamy <antoine.adamy@gmail.com>
2023-08-25 20:08:36 +0200

    good

--- srcs/backend/src/other/messages.service.ts
+++ srcs/backend/src/other/messages.service.ts
@@ -1,32 +1,30 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Message } from '../entities/message.entity';
+// import { Message } from '../entities/message.entity';
 
 @Injectable()
 export class MessagesService {
   constructor(
-    @InjectRepository(Message)
-    private messagesRepository: Repository<Message>,
   ) { }
 
-  async getAllMessages(): Promise<Message[]> {
-    return await this.messagesRepository.find();
-  }
+  // async getAllMessages(): Promise<Message[]> {
+  //   return await this.messagesRepository.find();
+  // }
 
-  async getMessageById(id: number) {
-    const message = await this.messagesRepository.findOne({
-      where: {
-        id: id,
-      },
-    });
-    if (message) {
-      return message;
-    }
-    throw new NotFoundException('Could not find the message');
-  }
+  // async getMessageById(id: number) {
+  //   const message = await this.messagesRepository.findOne({
+  //     where: {
+  //       id: id,
+  //     },
+  //   });
+  //   if (message) {
+  //     return message;
+  //   }
+  //   throw new NotFoundException('Could not find the message');
+  // }
 
-  async createMessage(messageData: Message): Promise<Message> {
-    return await this.messagesRepository.save(messageData);
-  }
+  // async createMessage(messageData: Message): Promise<Message> {
+  //   return await this.messagesRepository.save(messageData);
+  // }
 }
