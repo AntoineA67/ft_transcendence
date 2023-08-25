6bb2c6e4d5417772ca206a56511a7bb62a48a1b3
Hsin Yu Liu <dewseileaf@gmail.com>
2023-08-25 13:32:29 +0200

    sidebar perfect now

--- srcs/frontend/src/pages/Sidebar.tsx
+++ srcs/frontend/src/pages/Sidebar.tsx
@@ -21,62 +21,117 @@ type userContext = {
 }
 
 export default function Sidebar() {
-	//fetch user info
 	const [nickname, setNickname] = useState('Intelligent Seagul');
 	const [bio, setBio] = useState('Gaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
 	const [avatar, setAvatar] = useState('../assets/Friend.svg');
 
 	const fetchUser = async () => {
-		//fetch nickname
-		//fetch bio
-		//fetch avatar url
-		//set all
+		//fetch
 	}
 
 	useEffect(() => {
-    	fetchUser();
+		fetchUser();
 	}, []);
-	
-	
+
 	const handleClick = (e: React.MouseEvent<HTMLElement>) => {
 		const clicked = e.currentTarget as HTMLElement;
 		const sidebar = document.getElementById('sidebar-ul');
-		if (sidebar == null) return ;
+		if (sidebar == null) return;
 		let item: HTMLElement | null = sidebar.firstChild as HTMLElement;
-		
+
 		while (item) {
-			item.classList.remove("sidebar-item-magenta");
+			item.classList.remove("active");
 			item = item.nextSibling as HTMLElement;
 		}
-		clicked.classList.add("sidebar-item-magenta");
+		clicked.classList.add("active");
 	}
 
 	return (
-		<div className="scrollbar d-flex" >
-			<div className="sidebar">
-				<nav className="navbar navbar-nav">
-					<ul id="sidebar-ul" className="ps-0">
-						<li className='sidebar-item sidebar-item-magenta' onClick={handleClick}>
-							<Link to="."><img src={Home}/></Link>
+		<div className="container-fluid">
+			<div className="row">
+				
+				<div className="col-sm min-vh-100"> 
+					<Outlet context={{
+						nickname, bio, avatar, setNickname, setBio, setAvatar
+					} satisfies userContext} />
+				</div>
+
+				<div className="col-sm-auto sticky-bottom order-sm-first" style={{backgroundColor: "black"}}>
+					<ul className="nav navbar navbar-expand flex-sm-column justify-content-around gap-sm-4" 
+						id="sidebar-ul" role="navigation" >
+						<li className='nav-item' onClick={handleClick}>
+							<Link to="."><img src={Home} /></Link>
 						</li>
-						<li className='sidebar-item' onClick={handleClick}>
+						<li className="nav-item" onClick={handleClick}>
 							<Link to="."><img src={Search} /></Link>
 						</li>
-						<li className='sidebar-item' onClick={handleClick}>
+						<li className="nav-item" onClick={handleClick}>
 							<Link to="."><img src={Friend} /></Link>
 						</li>
-						<li className='sidebar-item' onClick={handleClick}>
+						<li className="nav-item" onClick={handleClick}>
 							<Link to="."><img src={Chat} /></Link>
 						</li>
-						<li className='sidebar-item' onClick={handleClick}>
+						<li className="nav-item" onClick={handleClick}>
 							<Link to="."><img src={Game} /></Link>
 						</li>
 					</ul>
-				</nav>
+				</div>
+
 			</div>
-			<Outlet context={{ nickname, bio, avatar, setNickname, setBio, setAvatar 
-				} satisfies userContext}/>
 		</div>
+
+
+		// <>
+
+			// <ul className="nav navbar navbar-expand flex-sm-column justify-content-around" id="sidebar-ul" role="">
+			// 	<li className='nav-item active' onClick={handleClick}>
+			// 		<Link to="."><img src={Home}/></Link>
+			// 	</li>
+			// 	<li  className="nav-item" onClick={handleClick}>
+			// 		<Link to="."><img src={Search} /></Link>
+			// 	</li>
+			// 	<li className="nav-item" onClick={handleClick}>
+			// 		<Link to="."><img src={Friend} /></Link>
+			// 	</li>
+			// 	<li className="nav-item" onClick={handleClick}>
+			// 		<Link to="."><img src={Chat} /></Link>
+			// 	</li>
+			// 	<li className="nav-item" onClick={handleClick}>
+			// 		<Link to="."><img src={Game} /></Link>
+			// 	</li>
+			// </ul>
+
+
+		// 	<Outlet context={{ nickname, bio, avatar, setNickname, setBio, setAvatar
+		//  		} satisfies userContext}/>
+		// </>
+
+
+		// <div className="scrollbar d-flex" >
+		// 	<div className="sidebar">
+		// 		<nav className="navbar navbar-nav">
+		// 			<ul id="sidebar-ul" className="ps-0">
+		// 				<li className='sidebar-item sidebar-item-magenta' onClick={handleClick}>
+		// 					<Link to="."><img src={Home}/></Link>
+		// 				</li>
+		// 				<li className='sidebar-item' onClick={handleClick}>
+		// 					<Link to="."><img src={Search} /></Link>
+		// 				</li>
+		// 				<li className='sidebar-item' onClick={handleClick}>
+		// 					<Link to="."><img src={Friend} /></Link>
+		// 				</li>
+		// 				<li className='sidebar-item' onClick={handleClick}>
+		// 					<Link to="."><img src={Chat} /></Link>
+		// 				</li>
+		// 				<li className='sidebar-item' onClick={handleClick}>
+		// 					<Link to="."><img src={Game} /></Link>
+		// 				</li>
+		// 			</ul>
+		// 		</nav>
+		// 	</div>
+			// <Outlet context={{ nickname, bio, avatar, setNickname, setBio, setAvatar 
+			// 	} satisfies userContext}/>
+		// </div>
 	);
 }
 
