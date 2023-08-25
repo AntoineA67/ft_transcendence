763430c5af218886c6d4f1675eada7b859993504
Hsin Yu Liu <dewseileaf@gmail.com>
2023-08-25 11:19:15 +0200

    profile setting page

--- srcs/frontend/src/pages/ProfileSetting.tsx
+++ srcs/frontend/src/pages/ProfileSetting.tsx
@@ -1,6 +1,8 @@
 import '../styles/ProfileSetting.css';
-import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link, Outlet } from "react-router-dom";
 
 export function Title({title}: {title: string}) {
@@ -12,18 +14,43 @@ export function Title({title}: {title: string}) {
 	);
 } 
 
+export function ChangePassword() {
+	return (
+		<div></div>
+	);
+}
 
 export default function ProfileSetting() {
 	return (
-		<Stack className="mb-5 d-flex flex-column align-items-center" style={{ color: "white", border: "2px solid white" }}>
-			<Title title="Setting"></Title>
-			<Link to="." className='link-text link fs-5 mt-4'>Change password </Link>
-			<Link to="." className='link-text link fs-5 mt-4'>Double Authenticate </Link>
-			<Link to="." className='link-text link fs-5 mt-4'>Change password </Link>
-			<button className="btn btn-outline-secondary mt-auto w-50" >
-				Log out
-			</button>
-
-		</Stack>
+		// <div style={{height: "100%", width: "100%"}}>
+		// 	<div style={{backgroundColor: "black", color: "white"}}>hello</div>
+		// 	<Container style={{border: "1px solid white"}}>
+		// 		<Row className="justify-content-center h-100">
+		// 			{/* <Title title="Setting"></Title> */}
+		// 			<Col sm="6" lg="4" className="d-flex flex-column">
+		// 				<Link to="." className='link-text link fs-5 mt-4'>Change password </Link>	
+		// 				<Link to="." className='link-text link fs-5 mt-4'>Double Authenticate </Link>
+		// 				<Link to="." className='link-text link fs-5 mt-4'>Change password </Link>
+		// 				<button className="btn btn-outline-secondary mt-auto w-100" >
+		// 					Log out
+		// 				</button>
+		// 			</Col>
+		// 		</Row>
+		// 	</Container>
+		// </div>
+			// <Stack gap={2} className="col-md-7 mx-auto" style={{ color: "white", border: "2px solid white" }}>
+		<>
+			<Stack className="mb-5 d-flex flex-column align-items-center" style={{ color: "white", border: "2px solid white" }}>
+				
+				<Title title="Setting"></Title>
+				<Link to="." className='link-text link fs-5 mt-4 d-inline'>Change password </Link>
+			
+				<Link to="." className='link-text link fs-5 mt-4'>Double Authenticate </Link>
+				<Link to="." className='link-text link fs-5 mt-4'>Change password </Link>
+				<button className="btn btn-outline-secondary mt-auto w-50" >
+					Log out
+				</button>
+			</Stack>
+		</>
 	);
 }
