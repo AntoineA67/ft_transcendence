05d88739366aa5fd8843c4721c3e2c3160086805
Hsin Yu Liu <dewseileaf@gmail.com>
2023-08-27 15:13:34 +0200

    pie char wip

--- srcs/frontend/src/pages/Stat.tsx
+++ srcs/frontend/src/pages/Stat.tsx
@@ -69,7 +69,70 @@ function AchieveContent() {
 }
 
 function PieChart() {
+	
+	// fetch data
+	const win = 300;
+	const lose = 100;
+	const total = win + lose;
+	
+	function gradientDoghnut(xc:number, yc:number, r:number) {
+		
+		const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+		if (!canvas) return ;
+		const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+		if (!ctx) return ;
+		
+		const colors = ['#fa34c3', '#34fafa']; //magenta cyan
+		const data = [win, lose];
+		let start = (2 * Math.PI) * (-1 / 4);
+		let gradient = null;
+		let startColor = null;
+		let	endColor = null;
+		
+		for (let i = 0; i < 2; i++) {
+			let degree = (2 * Math.PI) * (data[i] / total);
+			console.log(`${2 * Math.PI} ${data[i]}  ${total} ${degree}`);
+			
+			// x start / end of the next arc to draw
+			let xStart = xc + Math.cos(start) * r;
+			let xEnd = xc + Math.cos(start + degree) * r;
+			// y start / end of the next arc to draw
+			let yStart = yc + Math.sin(start) * r;
+			let yEnd = yc + Math.sin(start + degree) * r;
+			
+			startColor = colors[i];
+			endColor = colors[(i + 1) % colors.length];
+			gradient = ctx.createLinearGradient(xStart, yStart, xEnd, yEnd);
+			gradient.addColorStop(0, endColor);
+			gradient.addColorStop(0.5, startColor);
+			gradient.addColorStop(1.0, endColor);
+			
+			ctx.beginPath();
+			ctx.strokeStyle = gradient;
+			ctx.arc(xc, yc, r, start, start + degree, true);
+			ctx.lineWidth = 10;
+			ctx.stroke();
+			ctx.closePath();
 
+			start += degree;
+		}
+	}
+
+	useEffect(() => gradientDoghnut(100, 100, 60), []);
+	
+	return (
+		<Container className='my-5' 
+			style={{border: '1px solid red', color: 'white'}}>
+			<div className='row'>
+				<div className='col'>
+					<canvas id="canvas" width='200' height='200' style={{border: '1px solid white'}}></canvas>
+				</div>
+				<div className='col'>
+					stat
+				</div>
+			</div>
+		</Container>
+	);
 }
 
 export default function Stat() {
@@ -85,8 +148,9 @@ export default function Stat() {
 
 	return (
 		<>
+			<PieChart></PieChart>
 			{/* small screen  */}
-			<Container className="mb-5 pb-5 d-sm-none" 
+			<Container className="d-sm-none" 
 				style={{color: "white", border: "1px solid white"}}>
 				<div className="row">
 					<div className="col-6">
