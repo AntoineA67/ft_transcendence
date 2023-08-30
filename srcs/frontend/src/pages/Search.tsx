dbacf16404fdbf5bbcb15d96fa3212fc2a8384b2
Florian <git@florian-a.dev>
2023-08-30 11:56:18 +0200

    styilisation search bar

--- srcs/frontend/src/pages/Search.tsx
+++ srcs/frontend/src/pages/Search.tsx
@@ -10,14 +10,28 @@ import { Link, Outlet } from "react-router-dom";
 
 
 import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+
+export function SearchBar() {
+	return (
+		<div className='d-flex w-100 align-items-center' style={{ backgroundColor: "black"}}>
+			<Link to=".."><button className='goBack'></button></Link>
+			<Form.Control type="text" placeholder="Search anything" />
+		</div>
+	);
+}
 
 export function Search() {
 	return (
 		<>
 
+<Container fluid className='px-0 h-75'>
+			<SearchBar />
 
-			<Form.Control type="text" placeholder="Search anything" />
+
 			<Outlet />
+
+			</Container>
 		</>
 	)
 }
