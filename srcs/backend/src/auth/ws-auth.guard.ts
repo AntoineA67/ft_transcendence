7241947d5eef34554c961aa34a26c5dfa84be88a
Antoine Adamy <antoine.adamy@gmail.com>
2023-09-01 20:33:03 +0200

    requete ws authentifiee et verifiee en back !

--- srcs/backend/src/auth/ws-auth.guard.ts
+++ srcs/backend/src/auth/ws-auth.guard.ts
@@ -1,19 +1,18 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { Observable } from "rxjs";
 import { AuthGuard } from "@nestjs/passport";
 import { Reflector } from "@nestjs/core";
 import { IS_PUBLIC_KEY } from "./public.decorator";
+import { jwtConstants } from "./constants";
+import { verify } from "jsonwebtoken";
+import { UsersService } from "src/users/users.service";
 
 @Injectable()
 export class WsJwtGuard extends AuthGuard('ws-jwt') {
-	constructor(private reflector: Reflector) {
-		super();
-	}
+	constructor(private reflector: Reflector) { super() }
 
 	canActivate(context: ExecutionContext) {
-		const auth = context.getArgs()
-		console.log("auth: ", auth)
 		const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
 			context.getHandler(),
 			context.getClass(),
@@ -21,6 +20,25 @@ export class WsJwtGuard extends AuthGuard('ws-jwt') {
 		if (isPublic) {
 			return true;
 		}
-		return super.canActivate(context);
+
+		// const auth = context.switchToWs()
+		// const bearerToken = context.getArgs()[0].handshake.headers.authorization.split(' ')[1];
+		// const x = verify(bearerToken, jwtConstants.secret)
+		// console.log("verify: ", x)
+		// console.log("auth: ", bearerToken)
+		// const payload = bearerToken;
+
+		// console.log('jwt validate', payload);
+		// const user = this.userService.getUserByUsername(payload.sub);
+		// console.log('jwt validate user', user);
+
+		// if (!user) throw new UnauthorizedException('Please log in to continue');
+
+		return super.canActivate(context)
+		// return {
+		// 	id: payload.sub,
+		// 	email: payload.email,
+		// 	login: payload.login,
+		// };
 	}
 }
