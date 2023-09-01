15e5ccef5d9983d83a924588a33fe39d6b01e9f0
Antoine Adamy <antoine.adamy@gmail.com>
2023-09-01 18:38:51 +0200

    guard en cours

---
+++ srcs/backend/src/auth/ws-auth.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+import { AuthGuard } from "@nestjs/passport";
+import { Reflector } from "@nestjs/core";
+import { IS_PUBLIC_KEY } from "./public.decorator";
+
+@Injectable()
+export class WsJwtGuard extends AuthGuard('ws-jwt') {
+	constructor(private reflector: Reflector) {
+		super();
+	}
+
+	canActivate(context: ExecutionContext) {
+		const auth = context.switchToWs().getClient()
+		console.log("auth: ", auth)
+		const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+			context.getHandler(),
+			context.getClass(),
+		]);
+		if (isPublic) {
+			return true;
+		}
+		return super.canActivate(context);
+	}
+}
