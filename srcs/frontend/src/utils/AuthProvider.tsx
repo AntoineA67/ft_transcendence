ae46046713ab1ed37c7afe7e2cf026928669ae61
Hsin-yu Liu <hsliu@z2r6p4.42lyon.fr>
2023-09-04 15:02:47 +0200

    protcted route ok

--- srcs/frontend/src/utils/AuthProvider.tsx
+++ srcs/frontend/src/utils/AuthProvider.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, ReactComponentElement, useContext, useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
 type auth = {
 	id: string,
@@ -6,18 +7,28 @@ type auth = {
 	avatar: string,
 	token: string,
 	refreshToken: string,
+	// socket
 }
 
+export const AuthContext = createContext<any>(null);
 
-const AuthContext = createContext({});
+export function AuthProvider({children}: any) {
+	const [auth, setAuth] = useState<any>(null);
 
-function AuthProvider({children}: any) {
-	const [auth, setAuth] = useState({});
-
-	
 	return (
 		<AuthContext.Provider value={{auth, setAuth}}>
 			{children}
 		</AuthContext.Provider>
+	);
+}
+
+export function Protected({children}: any) {
+	const { auth } = useContext(AuthContext);
+	return (
+		auth ? (
+			<Outlet />
+		) : (
+			<Navigate to="/login" replace={true}/>
+		)
 	);
 }
