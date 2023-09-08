6b94381ff6c83bded9a804ce88c3ccf75ec40f09
Hsin-yu Liu <hsliu@z1r12p1.42lyon.fr>
2023-09-08 18:09:21 +0200

    more data

--- srcs/backend/src/prisma/seed.ts
+++ srcs/backend/src/prisma/seed.ts
@@ -5,125 +5,261 @@ async function main() {
   //create four user
 	const sasha = await prisma.user.create({
     data: {
-		id: 12345,
+		id: 1000,
 		username: 'Sasha',
 		email: 'sasha@prisma.io',
 		password: 'sashapass',
 		bio: 'Hello guys',
     },
   });
+  
   const kay = await prisma.user.create({
     data: {
-		id: 12346,
+		id: 1001,
 		username: 'Kay',
 		email: 'kay@prisma.io',
 		password: 'kaypass',
 		bio: 'Hello, good morning',
     },
   })
+  
   const jin = await prisma.user.create({
     data: {
-		id: 12347,
+		id: 1002,
 		username: 'Jin',
 		email: 'jin@prisma.io',
 		password: 'jinpass',
 		bio: 'Hello coucou',
     },
   })
+  
   const ting = await prisma.user.create({
     data: {
-		id: 12348,
+		id: 1003,
 		username: 'Ting',
 		email: 'ting@prisma.io',
 		password: 'tingpass',
 		bio: 'Heyyyyyyyyy',
     },
   })
+  
+  const flo = await prisma.user.create({
+    data: {
+		id: 1004,
+		username: 'Florian',
+		email: 'florian@prisma.io',
+		password: 'flopass',
+		bio: 'Coucou',
+    },
+  });
+  
+  const antoine = await prisma.user.create({
+    data: {
+		id: 1005,
+		username: 'Antoine',
+		email: 'antoine@prisma.io',
+		password: 'antoinepass',
+		bio: 'salut',
+    },
+  });
+  
+  const alric = await prisma.user.create({
+    data: {
+		id: 1006,
+		username: 'Alric',
+		email: 'alric@prisma.io',
+		password: 'alricpass',
+		bio: 'ciao',
+    },
+  });
+  
   //create friendship
   const friend1 = await prisma.friendship.create({
 	data: {
-		id: 54321,
+		id: 2001,
 		friends: {
-			connect: [{id: 12345}, {id: 12346}]
+			connect: [{id: 1000}, {id: 1001}]
 		}
 	}
   })
   const friend2 = await prisma.friendship.create({
 	data: {
-		id: 54321,
+		id: 2001,
 		friends: {
-			connect: [{id: 12345}, {id: 12347}]
+			connect: [{id: 1000}, {id: 1002}]
 		}
 	}
   })
+  
   const friend3 = await prisma.friendship.create({
 	data: {
-		id: 54321,
+		id: 2003,
 		friends: {
-			connect: [{id: 12345}, {id: 12348}]
+			connect: [{id: 1000}, {id: 1003}]
 		}
 	}
   })
+  
   //a friend request
   const friendReq = await prisma.friendRequest.create({
 	data: {
-		id: 123321,
-		user: {connect: {id: 12346}},
-		possibleFriend: {connect: {id: 12347}},
+		id: 3001,
+		user: {connect: {id: 1000}},
+		possibleFriend: {connect: {id: 1004}},
 		status: 'pending'
 	}
   })
   
+//   //a private chat room
+//   const channel = await prisma.room.create({
+// 	data: {
+// 		id: 4001,
+// 		isChannel: true,
+// 		owner: {connect: {id: 1005}},
+// 		title: 'transcendence', 
+// 		private: true, 
+// 		password: '123',
+// 		message: {
+// 			create: [
+// 				{
+// 					id: 5001,
+// 					message: 'hi where are you',
+// 					send_date: new Date(),
+// 					user: {connect: {id: 1000}},   
+// 				},
+// 				{
+// 					id: 5002,
+// 					message: 'At campus',
+// 					send_date: new Date(),
+// 					user: {connect: {id: 1005}}, 
+// 				},
+// 				{
+// 					id: 5003,
+// 					message: 'we are going to 42',
+// 					send_date: new Date(),
+// 					user: {connect: {id: 1006}}, 
+// 				}
+// 			]
+// 		},
+// 		members: {
+// 			create: [
+// 				{
+// 					id: 6001, 
+// 					user: {connect: {id: 1000}}
+// 				},
+// 				{
+// 					id: 6002, 
+// 					user: {connect: {id: 1004}}
+// 				}, 
+// 				{
+// 					id: 6005, 
+// 					user: {connect: {id: 1005}}
+// 				},
+// 				{
+// 					id: 6006, 
+// 					user: {connect: {id: 1006}}
+// 				}
+// 			]
+// 		}
+// 	}
+//   })
   
-  //a private chat room
-  const privateChatRoom = await prisma.room.create({
+	const privateChat = await prisma.room.create({
+		data: {
+			id: 4002,
+			isChannel: false,
+			message: {
+				create: [
+					{
+						id: 5004,
+						message: 'Hello!',
+						send_date: new Date(),
+						user: {connect: {id: 1000}},    
+					},
+					{
+						id: 5005,
+						message: 'Hi what are you doing!',
+						send_date: new Date(),
+						user: {connect: {id: 1001}},  
+					},
+					{
+						id: 5006,
+						message: 'eating',
+						send_date: new Date(),
+						user: {connect: {id: 1000}},  
+					}
+				]
+			},
+			members: {
+				create: [
+					{
+						id: 6003, 
+						user: {connect: {id: 1000}}
+					},
+					{
+						id: 6004, 
+						user: {connect: {id: 1001}}
+					}
+				]
+			}
+		}
+	  })
+  
+  const achieve = await prisma.achievement.create({
 	data: {
-		id: 9,
-		isChannel: false,
-		message: {
+		id: 7001,
+		user: {connect: {id: 1000}}, 
+		firstWin: true, 
+		winTenGames: true,
+		loggedAWeek: true
+	}
+  })
+  
+  const game1 = await prisma.game.create({
+	data: {
+		id: 8001, 
+		finish: true, 
+		start_date: new Date(),
+		end_date: new Date(),
+		score: '4:3',
+		players: {
 			create: [
 				{
-					id: 1,
-					message: 'Hello!',
-					send_date: new Date(),
-					user: {connect: {id: 12345}},    
+					id: 9001, 
+					win: true, 
+					userId: 1000
 				},
 				{
-					id: 2,
-					message: 'Hi what are you doing!',
-					send_date: new Date(),
-					user: {connect: {id: 12346}},  
-				},
-				{
-					id: 1,
-					message: 'eating',
-					send_date: new Date(),
-					user: {connect: {id: 12345}},  
-				}
-			]
-		},
-		members: {
-			create: [
-				{
-					id: 123, 
-					user: {connect: {id: 12345}},
-				},
-				{
-					id: 124, 
-					user: {connect: {id: 12346}},
+					id: 9002, 
+					win: false,
+					userId: 1001
 				}
 			]
 		}
 	}
   })
   
-  const achieve = await prisma.achievement.create({
+  const game2 = await prisma.game.create({
 	data: {
-		id: 11111,
-		user: {connect: {id: 12345}}, 
-		firstWin: true, 
-		winTenGames: true,
-		loggedAWeek: true
+		id: 8002, 
+		finish: true, 
+		start_date: new Date(),
+		end_date: new Date(),
+		score: '2:3',
+		players: {
+			create: [
+				{
+					id: 9003, 
+					win: false, 
+					userId: 1000
+				},
+				{
+					id: 9004, 
+					win: true,
+					userId: 1001
+				}
+			]
+		}
 	}
   })
 }
