dfac0c781b475d6d0d11e18e9a34287470ecec45
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-12 17:37:41 +0200

    wip

--- srcs/backend/src/users/users.service.ts
+++ srcs/backend/src/users/users.service.ts
@@ -5,6 +5,12 @@ import { Prisma, OnlineStatus, ReqState } from '@prisma/client'
 const user = Prisma.validator<Prisma.UserDefaultArgs>()({})
 export type User = Prisma.UserGetPayload<typeof user>
 
+const game = Prisma.validator<Prisma.GameDefaultArgs>()({})
+export type Game = Prisma.GameGetPayload<typeof game>
+
+const player = Prisma.validator<Prisma.PlayerDefaultArgs>()({})
+export type Player = Prisma.PlayerGetPayload<typeof player>
+
 @Injectable()
 export class UsersService {
 	constructor(private prisma: PrismaService) {}
@@ -20,31 +26,25 @@ export class UsersService {
 		});
 	}
 
-	// get a single user (frontend)
-	async getUserByNick(nick: string): Promise<any> {
+	// get info on another user (frontend)
+	async getUserByNick(myId: number, nick: string): Promise<any> {
 		let user = await this.prisma.user.findUnique({
 				where: {
 					username: nick
 				},
 				include: {
-					gameHistory: true,
 					friend: true,
 					sendFriendReq: true,
 					recvFriendReq: true,
 					block: true, 
 				}
 		});
-		
 		if (!user) return ({error: 'user not found'})
-		user.sendFriendReq.filter((x) => x.status == ReqState.PENDING)
-		user.recvFriendReq.filter((x) => x.status == ReqState.PENDING)
 		delete user.email;
 		delete user.password;
 		delete user.u2fHash;
 		delete user.otpHash;
 		delete user.activated2FA;
-		console.log(user);
-
 		return user;
 	}
 
@@ -88,7 +88,6 @@ export class UsersService {
 		}
 		return ({nickname: user.username});
 	}
-
 
 	//dont touch
 	async getUserByUsername(username: string) {
