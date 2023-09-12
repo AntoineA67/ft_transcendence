4a5c1374aed23b14a4e200e7f7a3223f3d6e2123
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-12 12:08:21 +0200

    so that it makes

--- srcs/backend/src/prisma/seeds/games.ts
+++ srcs/backend/src/prisma/seeds/games.ts
@@ -2,16 +2,16 @@
 export const games = [
 	{
 		id: 1,
-		// finish: true,
-		// start_date: new Date(),
-		// end_date: new Date(),
+		finish: true,
+		start_date: new Date(),
+		end_date: new Date(),
 		score: '4:3',
 	},
 	{
 		id: 2,
-		// finish: true,
-		// start_date: new Date(),
-		// end_date: new Date(),
+		finish: true,
+		start_date: new Date(),
+		end_date: new Date(),
 		score: '2:3',
 	}
 ]
