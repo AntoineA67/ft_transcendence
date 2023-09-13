a33ab10a73c2d19ac4826a77ae47c090880c41c3
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-13 19:54:13 +0200

    wip

--- srcs/backend/src/block/block.module.ts
+++ srcs/backend/src/block/block.module.ts
@@ -3,9 +3,10 @@ import { BlockService } from './block.service';
 import { BlockController } from './block.controller';
 import { BlockGateway } from './block.gateway';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   providers: [BlockService, BlockGateway],
   controllers: [BlockController],
 })
