e8c29670a5852640160f06653729c32b0eee1232
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-13 13:24:13 +0200

    break friend module into two module

--- srcs/backend/src/friendrequest/friendrequest.controller.ts
+++ srcs/backend/src/friendrequest/friendrequest.controller.ts
@@ -6,32 +6,5 @@ import { FriendRequest, Prisma } from '@prisma/client';
 export class FriendRequestController {
   constructor(private readonly friendRequestService: FriendRequestService) {}
 
-  @Post()
-  async createFriendRequest(@Body() data: Prisma.FriendRequestCreateInput): Promise<FriendRequest> {
-    return this.friendRequestService.createFriendRequest(data);
-  }
-
-  @Get(':id')
-  async getFriendRequestById(@Param('id') id: string): Promise<FriendRequest> {
-    const requestId = parseInt(id, 10);
-    const request = await this.friendRequestService.getFriendRequestById(requestId);
-    return request;
-  }
-
-  @Get()
-  async getAllFriendRequests(): Promise<FriendRequest[]> {
-    return this.friendRequestService.getAllFriendRequests();
-  }
-
-  @Put(':id')
-  async updateFriendRequest(@Param('id') id: string, @Body() data: Prisma.FriendRequestUpdateInput): Promise<FriendRequest | null> {
-    const requestId = parseInt(id, 10);
-    return this.friendRequestService.updateFriendRequest(requestId, data);
-  }
-
-  @Delete(':id')
-  async deleteFriendRequest(@Param('id') id: string): Promise<FriendRequest | null> {
-    const requestId = parseInt(id, 10);
-    return this.friendRequestService.deleteFriendRequest(requestId);
-  }
+  
 }
