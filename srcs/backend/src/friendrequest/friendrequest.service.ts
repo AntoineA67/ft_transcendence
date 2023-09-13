8f60a7e8aa405d23ba783d2f2a1bf29a8522b81a
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-13 14:33:37 +0200

    service in progress

--- srcs/backend/src/friendrequest/friendrequest.service.ts
+++ srcs/backend/src/friendrequest/friendrequest.service.ts
@@ -1,68 +1,86 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { ReqState } from '@prisma/client';
+import { Prisma, ReqState } from '@prisma/client';
 import { UsersService } from 'src/users/users.service';
+import { FriendshipService } from 'src/friendship/friendship.service';
 import { FriendReplyDto } from './dto/FriendReplyDto';
 import { FriendReqDto } from './dto/FriendReqDto';
 
+const friendReq = Prisma.validator<Prisma.FriendRequestDefaultArgs>()({})
+export type FriendReq = Prisma.FriendRequestGetPayload<typeof friendReq>
+
+
 @Injectable()
 export class FriendRequestService {
+	
 	constructor(
-		private usersService: UsersService,
+		private readonly usersService: UsersService,
+		private readonly friendshipService: FriendshipService,
 		private prisma: PrismaService
-	) { }
+		) {}
 
-	async replyFriendReq(reply: FriendReplyDto) {
+	async sendFriendReq(req: FriendReqDto): Promise<Boolean> {
+		const sendId = await this.usersService.getIdByNick(req.sendNick);
+		const recvId = await this.usersService.getIdByNick(req.recvNick);
+		if (!sendId || !recvId) return (false);
+		// check block or not
+		// check if they are already friends
+		const isFriend = await this.friendshipService.isFriend(req.sendNick, req.recvNick);
+		if (isFriend) return (true);
+		//check if there are pending requests
+		const pendings = await this.getPendingReq(req.sendNick, req.recvNick);
+		if (pendings.length != 0) return (true);
+		this.prisma.friendRequest.create({
+			data: {
+				userId: sendId,
+				possibleFriendId: recvId
+			}
+		})
+		return (true);
+	}
+
+	async replyFriendReq(reply: FriendReplyDto): Promise<Boolean> {
 		const sendId = await this.usersService.getIdByNick(reply.sendNick);
 		const recvId = await this.usersService.getIdByNick(reply.recvNick);
 		const status = reply.reply == 'accept' ? ReqState.ACCEPT : ReqState.DECLINE;
-		if (!sendId) return ({ result: 'user not found' });
-		if (!recvId) return ({ result: 'user not found' });
-		const exist = await this.prisma.friendRequest.updateMany({
-			where: {
-				AND: [
-					{ userId: { equals: sendId } },
-					{ possibleFriendId: { equals: recvId } },
-					{ status: { equals: 'PENDING' } }
-				],
-			},
-			data: {
-				status: status
-			}
-		})
-		if (status == ReqState.ACCEPT) {
-			await this.prisma.friendship.create({
+		if (!sendId || !recvId) return (false);
+		try {
+			await this.prisma.friendRequest.updateMany({
+				where: {
+					AND: [
+						{ userId: { equals: sendId } },
+						{ possibleFriendId: { equals: recvId } },
+						{ status: { equals: 'PENDING' } }
+					],
+				},
 				data: {
-					friends: { connect: [{ id: sendId }, { id: recvId }] }
+					status: status
 				}
 			})
+		} catch (err: any) {console.log('err: replyFriendReq')};
+		if (status == ReqState.ACCEPT) {
+			await this.friendshipService.makeFriend(reply.sendNick, reply.recvNick);
 		}
 	}
 
-	async createFriendReq(req: FriendReqDto) {
-		const sendId = await this.usersService.getIdByNick(req.sendNick);
-		const recvId = await this.usersService.getIdByNick(req.recvNick);
-		if (!sendId) return ({ result: 'user not found' });
-		if (!recvId) return ({ result: 'user not found' });
-		// check block or not
-		// check if they are already friends
-		const exist = await this.prisma.friendRequest.findMany({
+	async getPendingReq(myNick: string, nick: string) {
+		const myId = await this.usersService.getIdByNick(myNick);
+		const id = await this.usersService.getIdByNick(nick);
+		if (!myId || !id) return ([]);
+		const pendings = await this.prisma.friendRequest.findMany({
 			where: {
 				AND: [
-					{ userId: { equals: sendId } },
-					{ possibleFriendId: { equals: recvId } },
+					{ userId: { equals: myId } },
+					{ possibleFriendId: { equals: id } },
 					{ status: { equals: 'PENDING' } }
 				],
 			},
+			include: {
+				user: {select: {username: true}},
+				possibleFriend: {select: {username: true}}
+			}
 		})
-		if (exist.length == 0) {
-			this.prisma.friendRequest.create({
-				data: {
-					userId: sendId,
-					possibleFriendId: recvId
-				}
-			})
-		}
-		return ({ result: 'success' });
-}
+		return (pendings);
+	}
+
 }
