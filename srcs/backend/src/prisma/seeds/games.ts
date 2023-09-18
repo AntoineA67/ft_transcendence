c906e584353d2d41c5d679ec99b315364dc19f38
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-18 23:53:26 +0200

    seed rewrite

--- srcs/backend/src/prisma/seeds/games.ts
+++ srcs/backend/src/prisma/seeds/games.ts
@@ -1,17 +1,39 @@
 
 export const games = [
 	{
-		id: 1,
 		finish: true,
 		start_date: new Date(),
 		end_date: new Date(),
 		score: '4:3',
+		players: {
+			create: [
+				{
+					user: { connect: { username: 'Sasha' } },
+					win: true,
+				}, 
+				{
+					user: { connect: { username: 'Florian' } },
+					win: false,
+				},
+			]
+		}
 	},
 	{
-		id: 2,
 		finish: true,
 		start_date: new Date(),
 		end_date: new Date(),
 		score: '2:3',
+		players: {
+			create: [
+				{
+					user: { connect: { username: 'Sasha' } },
+					win: false,
+				},
+				{
+					user: { connect: { username: 'Florian' } },
+					win: true,
+				},
+			]
+		}
 	}
 ]
