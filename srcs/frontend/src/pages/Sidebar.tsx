d95abd83241fe9d46ddfad455fa9a978b695b603
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-19 20:41:18 +0200

    user can modify nick and bio

--- srcs/frontend/src/pages/Sidebar.tsx
+++ srcs/frontend/src/pages/Sidebar.tsx
@@ -10,29 +10,8 @@ import Search from '../assets/Search.svg';
 import Friend from '../assets/Friend.svg';
 
 import { Outlet, useOutletContext, Link } from "react-router-dom";
-import Profile from './Profile';
-
-type userContext = {
-	nickname: string,
-	bio: string,
-	avatar: string,
-	setNickname: React.Dispatch<React.SetStateAction<string>>,
-	setBio: React.Dispatch<React.SetStateAction<string>>,
-	setAvatar: React.Dispatch<React.SetStateAction<string>>,
-}
 
 export default function Sidebar() {
-	const [nickname, setNickname] = useState('Intelligent Seagul');
-	const [bio, setBio] = useState('Gaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-	const [avatar, setAvatar] = useState('../assets/Friend.svg');
-
-	const fetchUser = async () => {
-		//fetch
-	}
-
-	useEffect(() => {
-		fetchUser();
-	}, []);
 
 	const handleClick = (e: React.MouseEvent<HTMLElement>) => {
 		const clicked = e.currentTarget as HTMLElement;
@@ -52,9 +31,7 @@ export default function Sidebar() {
 			<div className="row">
 				
 				<div className="col-sm vh-100 overflow-auto" style={{padding: "0"}}>  {/* min-vh-100 */}
-					<Outlet context={{
-						nickname, bio, avatar, setNickname, setBio, setAvatar
-					} satisfies userContext} />
+					<Outlet />
 				</div>
 				<div className="col-sm-auto sticky-bottom order-sm-first" style={{backgroundColor: "black"}}>
 					<ul className="nav navbar navbar-expand flex-sm-column justify-content-around gap-sm-4" 
@@ -79,62 +56,5 @@ export default function Sidebar() {
 
 			</div>
 		</div>
-
-
-		// <>
-
-			// <ul className="nav navbar navbar-expand flex-sm-column justify-content-around" id="sidebar-ul" role="">
-			// 	<li className='nav-item active' onClick={handleClick}>
-			// 		<Link to="."><img src={Home}/></Link>
-			// 	</li>
-			// 	<li  className="nav-item" onClick={handleClick}>
-			// 		<Link to="."><img src={Search} /></Link>
-			// 	</li>
-			// 	<li className="nav-item" onClick={handleClick}>
-			// 		<Link to="."><img src={Friend} /></Link>
-			// 	</li>
-			// 	<li className="nav-item" onClick={handleClick}>
-			// 		<Link to="."><img src={Chat} /></Link>
-			// 	</li>
-			// 	<li className="nav-item" onClick={handleClick}>
-			// 		<Link to="."><img src={Game} /></Link>
-			// 	</li>
-			// </ul>
-
-
-		// 	<Outlet context={{ nickname, bio, avatar, setNickname, setBio, setAvatar
-		//  		} satisfies userContext}/>
-		// </>
-
-
-		// <div className="scrollbar d-flex" >
-		// 	<div className="sidebar">
-		// 		<nav className="navbar navbar-nav">
-		// 			<ul id="sidebar-ul" className="ps-0">
-		// 				<li className='sidebar-item sidebar-item-magenta' onClick={handleClick}>
-		// 					<Link to="."><img src={Home}/></Link>
-		// 				</li>
-		// 				<li className='sidebar-item' onClick={handleClick}>
-		// 					<Link to="."><img src={Search} /></Link>
-		// 				</li>
-		// 				<li className='sidebar-item' onClick={handleClick}>
-		// 					<Link to="."><img src={Friend} /></Link>
-		// 				</li>
-		// 				<li className='sidebar-item' onClick={handleClick}>
-		// 					<Link to="."><img src={Chat} /></Link>
-		// 				</li>
-		// 				<li className='sidebar-item' onClick={handleClick}>
-		// 					<Link to="."><img src={Game} /></Link>
-		// 				</li>
-		// 			</ul>
-		// 		</nav>
-		// 	</div>
-			// <Outlet context={{ nickname, bio, avatar, setNickname, setBio, setAvatar 
-			// 	} satisfies userContext}/>
-		// </div>
-	);
-}
-
-export function useUser() {
-	return useOutletContext<userContext>();
+	)
 }
