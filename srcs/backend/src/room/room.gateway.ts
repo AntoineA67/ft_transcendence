dfddbaaca58936f85a9570759014c32236cf1e06
AlricPerso <alric.conte@gmail.com>
2023-09-20 19:01:47 +0200

    update message

--- srcs/backend/src/room/room.gateway.ts
+++ srcs/backend/src/room/room.gateway.ts
@@ -1,12 +1,33 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayDisconnect, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Server } from 'socket.io';
+import { OnGatewayConnection } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { MessageBody } from '@nestjs/websockets';
+import { ConnectedSocket } from '@nestjs/websockets';
+import { RoomService } from './room.service';
+import { Socket } from 'socket.io';
+import { users } from 'src/prisma/seeds/users';
+import { User } from 'src/users/users.service';
 
 @WebSocketGateway({ cors: true })
-export class RoomGateway {
-  @WebSocketServer()
-  server: Server;
+export class RoomGateway
+	implements OnGatewayConnection, OnGatewayDisconnect {
+	constructor(private readonly roomService: RoomService) { }
+	private logger: Logger = new Logger('RoomGateway');
 
-  handleRoomEvent(data: any) {
-    this.server.emit('roomEvent', data);
-  }
+	async handleConnection(client: Socket) {
+		const id: number = client.data.user.id;
+		client.join(id.toString());
+	}
+
+	async handleDisconnect(client: Socket) {
+		const id: number = client.data.user.id;
+		client.leave(id.toString());
+	}
+
+	@SubscribeMessage('getAllRoomsByUserid')
+	async handleMyProfile(@ConnectedSocket() client: Socket) {
+		const id: number = client.data.user.id;
+		return (await this.roomService.getAllRoomsByUserid(id));
+	}
 }
