95b56c5a50c110259167b15957c8b55025cfaf6f
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-25 20:10:50 +0200

    norm

--- srcs/backend/src/users/dto/UpdateUserDto.ts
+++ srcs/backend/src/users/dto/UpdateUserDto.ts
@@ -3,5 +3,6 @@ export class UpdateUserDto {
 	username?: string;
 	password?: string;
 	bio?: string;
+	avatar?: Buffer;
 	status?: 'ONLINE' | 'OFFLINE' | 'INGAME';
 }
--- srcs/backend/src/users/users.controller.ts
+++ srcs/backend/src/users/users.controller.ts
@@ -5,16 +5,23 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express'
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('users')
 export class UsersController {
-	constructor(private readonly usersService: UsersService) { }
+	constructor(private readonly usersService: UsersService, private prisma: PrismaService) {
+		this.logger.log('users controller')
+	}
 
 	private logger = new Logger('users');
 
-	@Post('newAvatar')
-	@UseInterceptors(FileInterceptor('avatar'))
-	uploadFile(@UploadedFile() file: Express.Multer.File) {
-		console.log(file);
-	}
+	// @Post('newAvatar')
+	// @UseInterceptors(FileInterceptor('avatar'))
+	// uploadFile(@UploadedFile() file: Express.Multer.File) {
+		
+	// 	console.log(file);
+		
+	// 	if (file.mimetype != 'image/jpeg') { return ('not jpeg') }
+	// 	return ('get it')
+	// }
 }
