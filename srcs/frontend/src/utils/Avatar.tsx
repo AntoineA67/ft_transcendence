986b1efc3cb23b52807cffa36f79bffde3219176
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-26 11:53:50 +0200

    online status ok

--- srcs/frontend/src/utils/Avatar.tsx
+++ srcs/frontend/src/utils/Avatar.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react"
 import { userType } from "../../types/user"
 import { useState } from "react"
+import DefaultAvatar from '../assets/defaultAvatar.svg'
+import Friend from '../assets/Friend.svg'
+import { socket } from "./socket"
 
 type avatarProp = {
 	size: number, 
@@ -10,21 +13,45 @@ type avatarProp = {
 export function Avatar({ size, user }: avatarProp) {
 	const [avatar, setAvatar] = useState('')
 	const [color, setColor] = useState('')
+	const [status, setStatus] = useState<'ONLINE' | 'OFFLINE' | 'INGAME'>(user.status);
 	
 	useEffect(() => {
-		
+		console.log(user);
 		// define behavior to user online status
-		
-		user.status == 'ONLINE' && setColor('green');
-		user.status == 'OFFLINE' && setColor('grey');
-		user.status == 'INGAME' && setColor('red');
-		if (!user.avatar) return ;
-		const base64 = btoa(
-			new Uint8Array(user.avatar)
+		function onOnline(id: number) {
+			user.id == id && setStatus('ONLINE');
+		}
+		function onOffline(id: number) {
+			user.id == id && setStatus('OFFLINE');
+		}
+		function onIngame(id: number) {
+			user.id == id && setStatus('INGAME');
+		}
+		socket.on('online', onOnline);
+		socket.on('offline', onOffline);
+		socket.on('ingame', onIngame);
+		// set Avatar
+		if (!user.avatar) {
+			setAvatar(DefaultAvatar);
+		} else {
+			const base64 = btoa(
+				new Uint8Array(user.avatar)
 				.reduce((data, byte) => data + String.fromCharCode(byte), '')
-		);
-		setAvatar(`data:image/jpeg;base64,${base64}`)
-	}, [user]);
+			);
+			setAvatar(`data:image/jpeg;base64,${base64}`)
+		}
+		return () => {
+			socket.off('online', onOnline);
+			socket.off('offline', onOffline);
+			socket.off('ingame', onIngame);
+		};
+	}, []);
+
+	useEffect(() => {
+		status == 'ONLINE' && setColor('green');
+		status == 'OFFLINE' && setColor('grey');
+		status == 'INGAME' && setColor('red');
+	}, [status])
 
 	return (
 		<>
@@ -32,6 +59,7 @@ export function Avatar({ size, user }: avatarProp) {
 				height: `${size / 6}px`,
 				width: `${size / 6}px`, 
 				borderRadius: '50%',
+				border: '2px solid white',
 				backgroundColor: color,
 				position: 'relative',
 				left: `${size / 3}px`, 
@@ -41,8 +69,8 @@ export function Avatar({ size, user }: avatarProp) {
 				className='p-0 m-0'
 				src={avatar} 
 				style={{
-					maxHeight: `${size}px`, 
-					maxWidth: `auto`, 
+					height: `${size}px`, 
+					width: `auto`, 
 					objectFit: 'contain', 
 					clipPath: 'circle()', 
 				}}
