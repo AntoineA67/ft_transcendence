85768932caa0cc99843b59f9a0bd3efb7dcf6635
alricwsl <alric.conte@gmail.com>
2023-09-27 15:47:10 +0200

    reel time message work on room

--- srcs/frontend/src/pages/Chat.tsx
+++ srcs/frontend/src/pages/Chat.tsx
@@ -2,7 +2,7 @@ import '../styles/ProfileSetting.css';
 import '../styles/Chat.css';
 import { Link, Outlet } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { socket } from '../utils/socket';
 import { useNavigate } from 'react-router-dom';
@@ -22,7 +22,7 @@ type Profile = {
 	id: number;
 	status: string;
 	username: string;
-  };
+};
 
 export function ChatBox() {
 	const { chatId } = useParams();
@@ -36,45 +36,51 @@ export function ChatBox() {
 		id: 0,
 		status: '',
 		username: '',
-	  });
+	});
 	const navigate = useNavigate();
+	const messagesEndRef = useRef<HTMLUListElement | null>(null);
 
 	useEffect(() => {
-	socket.connect();
-	socket.emit('getRoomData', chatId, (data: { messages: Message[], roomTitle: string }) => {
-		setRoomTitle(data.roomTitle);
-		setMessages(data.messages);
-		setLoading(false);
-		console.log('data', data);
-	});
+		socket.connect();
+		socket.emit('getRoomData', chatId, (data: { messages: Message[], roomTitle: string }) => {
+			setRoomTitle(data.roomTitle);
+			setMessages(data.messages);
+			setLoading(false);
+		});
 
-	socket.emit('MyProfile', (data: Profile) => {
-		console.log('Profile', data);
-		setProfile(data);
-	});
+		socket.emit('MyProfile', (data: Profile) => {
+			setProfile(data);
+		});
 
-	return () => {
-		socket.disconnect();
-	};
+		socket.on('messageSent', (newMessage: Message) => {
+			setMessages((prevMessages) => [...prevMessages, newMessage]);
+		});
+		return () => {
+			socket.disconnect();
+			socket.off('messageSent');
+		};
 	}, [chatId]);
+	console.log('new message', messages);
 
 	useEffect(() => {
-	if (!loading && roomTitle === '') {
-		navigate('/chat');
-	}
+		if (!loading && roomTitle === '') {
+			navigate('/chat');
+		}
 	}, [loading, roomTitle, navigate]);
-
-	if (loading) {
-	return <p>Loading...</p>;
-	}
 
 	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === 'Enter') {
-		if (mess.trim()) {
-			handleSendMessage();
-		}
+			if (mess.trim()) {
+				handleSendMessage();
+			}
 		}
 	};
+
+	useEffect(() => {
+		if (messagesEndRef.current) {
+		  messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+		}
+	  }, [messages]);
 
 	const handleClick = () => {
 		if (mess.trim()) {
@@ -84,53 +90,59 @@ export function ChatBox() {
 
 	const handleSendMessage = () => {
 		socket.emit('sendMessage', {
-		roomId: chatId,
-		content: mess,
-		userid: profile.id,
+			roomId: chatId,
+			content: mess,
+			userid: profile.id,
 		}, (response: any) => {
-		if (response.error) {
-			console.error('Erreur lors de l\'envoi du message :', response.error);
-		} else {
-			setMess('');
-		}
+			if (response.error) {
+				console.error('Erreur lors de l\'envoi du message :', response.error);
+			}
 		});
+		setMess('');
+		messagesEndRef.current?.scrollTo(0, messagesEndRef.current.scrollHeight);
 	};
 
 	const myMap = (message: Message, profile: Profile) => {
-		const classname = message.username === profile.username ? 'messageBlue' : 'messagePink';
-		const classuser = message.username === profile.username ? 'userBlue' : 'userPink';
+		const classname = message.userId === profile.id ? 'messageBlue' : 'messagePink';
+		const classuser = message.userId === profile.id ? 'userBlue' : 'userPink';
 		const formattedTime = new Date(message.send_date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
+		if (message.userId === profile.id) {
+			message.username = profile.username;
+		}
 		return (
-		  <div className="message-container" key={message.id}>
-			<strong className={`message ${classuser}`}>{message.username} - {formattedTime}</strong>
-			<div className={`message ${classname}`}>
-			  {message.message}
+			<div className="message-container" key={message.id}>
+				<strong className={`message ${classuser}`}>{message.username} - {formattedTime}</strong>
+				<div className={`message ${classname}`}>
+					{message.message}
+				</div>
 			</div>
-		  </div>
 		);
-	  };
+	};
 
 	return (
 		<div className="h-100 d-flex flex-column">
-			<div
+		<div
 			className="d-flex w-100 align-items-center p-1 ps-sm-5"
 			style={{ backgroundColor: 'black' }}
-			>
+		>
 			<span className="d-sm-none">
-				<Link to="..">
+			<Link to="..">
 				<button className="goBack"></button>
-				</Link>
+			</Link>
 			</span>
 			<h4 style={{ color: 'white', margin: 'auto 0' }}>{roomTitle}</h4>
-			</div>
-			<div className="p-5 flex-grow" style={{ overflowY: 'auto' }}>
-			<ul className="nostyleList d-flex flex-column" style={{ color: 'white' }}>
-				{messages.map((message) => myMap(message, profile))}
+		</div>
+		<div className="p-5 flex-grow" style={{ overflowY: 'auto' }}>
+			<ul
+			ref={messagesEndRef}
+			className="nostyleList d-flex flex-column"
+			style={{ color: 'white' }}
+			>
+			{messages.map((message) => myMap(message, profile))}
 			</ul>
-			</div>
-	
-			<div className="mb-5 mb-sm-0 p-3  d-flex align-items-center">
+		</div>
+		<div className="mb-5 mb-sm-0 p-3  d-flex align-items-center">
 			<input
 			className="p-2 flex-grow-1"
 			style={{ borderRadius: '10px' }}
@@ -139,9 +151,9 @@ export function ChatBox() {
 			onKeyDown={handleKeyDown}
 			/>
 			<button className="send-message" onClick={handleClick}></button>
-			</div>
 		</div>
-		);
+		</div>
+	);
 }
 
 type formProp = {
@@ -207,43 +219,43 @@ export function ChatList() {
 	const [rooms, setRooms] = useState<Rooms[]>([]);
 
 	useEffect(() => {
-	socket.connect();
-	socket.emit('getAllRoomsByUserid', (response: Rooms[]) => {
-		setRooms(response);
-	});
-	return () => {
-		socket.disconnect();
-	};
+		socket.connect();
+		socket.emit('getAllRoomsByUserid', (response: Rooms[]) => {
+			setRooms(response);
+		});
+		return () => {
+			socket.disconnect();
+		};
 	}, []);
 
 	const myMap = (room: Rooms) => {
 		return (
-		  <li key={room.title}>
-			<Link to={`/chat/${room.id}`} className='link-text' style={{ color: 'white' }}>
-			  <div className='chatListItem'>{room.title}</div>
-			</Link>
-		  </li>
+			<li key={room.title}>
+				<Link to={`/chat/${room.id}`} className='link-text' style={{ color: 'white' }}>
+					<div className='chatListItem'>{room.title}</div>
+				</Link>
+			</li>
 		);
-	  };
+	};
 
 	return (
-	<div className='w-100 h-100 d-flex flex-column'>
-		{page === 'newChat' && <NewChat setPage={setPage} />}
-		{page === 'chatList' && (
-		<>
-			<div className='d-flex w-100 align-items-center p-2 ps-4 ps-sm-2' style={{ backgroundColor: "black" }}>
-			<h4 style={{ color: "white", margin: "auto 0" }}>Chat</h4>
-			<button className='new-chat ms-auto' onClick={() => setPage('newChat')} />
-			</div>
+		<div className='w-100 h-100 d-flex flex-column'>
+			{page === 'newChat' && <NewChat setPage={setPage} />}
+			{page === 'chatList' && (
+				<>
+					<div className='d-flex w-100 align-items-center p-2 ps-4 ps-sm-2' style={{ backgroundColor: "black" }}>
+						<h4 style={{ color: "white", margin: "auto 0" }}>Chat</h4>
+						<button className='new-chat ms-auto' onClick={() => setPage('newChat')} />
+					</div>
 
-			<div className='flex-grow-1 pb-5 pb-sm-0' style={{ overflowY: 'auto' }}>
-			<ul className='nostyleList py-0' >
-				{rooms.map(myMap)}
-			</ul>
-			</div>
-		</>
-		)}
-	</div>
+					<div className='flex-grow-1 pb-5 pb-sm-0' style={{ overflowY: 'auto' }}>
+						<ul className='nostyleList py-0' >
+							{rooms.map(myMap)}
+						</ul>
+					</div>
+				</>
+			)}
+		</div>
 	);
 }
 
