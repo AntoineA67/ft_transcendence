1c3651e74117e21e6df1cb98b924754cfafeb02e
Hsin Yu Liu <dewseileaf@gmail.com>
2023-09-28 16:42:02 +0200

    friends page in progress

--- srcs/backend/src/users/users.module.ts
+++ srcs/backend/src/users/users.module.ts
@@ -3,9 +3,11 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersGateway } from './users.gateway';
+import { FriendshipService } from 'src/friendship/friendship.service';
+import { BlockService } from 'src/block/block.service';
 
 @Module({
-  providers: [UsersService, UsersGateway],
+  providers: [UsersService, UsersGateway, FriendshipService, BlockService],
   controllers: [UsersController],
   exports: [UsersService],
   imports: [PrismaModule],
