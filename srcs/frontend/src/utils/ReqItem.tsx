f6d0a8ad9ffbb08235376dc79d3b4d92e16ffacf
Hsin-yu Liu <hsliu@z1r2p5.42lyon.fr>
2023-10-09 16:09:52 +0200

    avatar auto update

--- srcs/frontend/src/utils/ReqItem.tsx
+++ srcs/frontend/src/utils/ReqItem.tsx
@@ -14,7 +14,7 @@ export function ReqItem({ user, linkTo, onAccept, onDecline }: ReqItemProp) {
 		<div
 			className='userItem d-flex flex-row align-items-center'>
 			<Link to={linkTo}>
-				<Avatar size={40} user={user} />
+				<Avatar size={40} user={{... user}} />
 			</Link>
 			<p className='m-0 ms-3' style={{color: 'white'}}>
 				{user.username}
