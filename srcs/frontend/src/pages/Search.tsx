ae76d90e19641a8e5e049bd0175b35c66dc109a3
Hsin-yu Liu <hsliu@z2r3p1.42lyon.fr>
2023-10-12 13:16:21 +0200

    all seems good

--- srcs/frontend/src/pages/Search.tsx
+++ srcs/frontend/src/pages/Search.tsx
@@ -14,6 +14,7 @@ export function SearchBar() {
 	const [temp, setTemp] = useState<userType[]>([]);
 	
 	useEffect(() => {
+		console.log('searchbar: ')
 		socket.emit('getAllUsers', (response: userType[]) => {
 			setList(response)
 			setTemp(response);
