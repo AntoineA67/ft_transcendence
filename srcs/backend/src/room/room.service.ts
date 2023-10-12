41a7df3ea7821d32d1eba0c3bd052abc7d79447b
Hsin Yu Liu <dewseileaf@gmail.com>
2023-10-12 21:36:50 +0200

    avatar style ok

--- srcs/backend/src/room/room.service.ts
+++ srcs/backend/src/room/room.service.ts
@@ -194,7 +194,7 @@ export class RoomService {
 	}
 
 	async createPrivateRoom(userId: number, username: string) {
-		const userprofile = await this.usersService.getUserProfileById(userId);
+		const userprofile = await this.usersService.getUserById(userId);
 		if (!userprofile)
 			return null;
 
