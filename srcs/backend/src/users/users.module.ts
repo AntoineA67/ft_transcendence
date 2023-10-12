3997cc2167eab657583490a5305e30df9bafb2c7
Hsin-yu Liu <hsliu@z2r3p1.42lyon.fr>
2023-10-12 13:16:21 +0200

    all seems good

--- srcs/backend/src/users/users.module.ts
+++ srcs/backend/src/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersGateway } from './users.gateway';
 
 @Module({
 	imports: [PrismaModule],
-	providers: [UsersService],
+	providers: [UsersService, UsersGateway],
 	controllers: [UsersController],
 	exports: [UsersService],
 })
