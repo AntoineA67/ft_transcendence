ad79e5ac5c0e58fce77d84c43aeccd767083e323
Hsin-yu Liu <hsliu@z4r12p1.42lyon.fr>
2023-10-17 17:35:41 +0200

    side bar remake

--- srcs/frontend/src/pages/Sidebar.tsx
+++ srcs/frontend/src/pages/Sidebar.tsx
@@ -9,51 +9,47 @@ import Home from '../assets/Home.svg';
 import Search from '../assets/Search.svg';
 import Friend from '../assets/Friend.svg';
 
-import { Outlet, useOutletContext, Link } from "react-router-dom";
+import { Outlet, useOutletContext, Link, useLocation } from "react-router-dom";
 
 export default function Sidebar() {
-
-	const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-		const clicked = e.currentTarget as HTMLElement;
-		const sidebar = document.getElementById('sidebar-ul');
-		if (sidebar == null) return;
-		let item: HTMLElement | null = sidebar.firstChild as HTMLElement;
-
-		while (item) {
-			item.classList.remove("magenta");
-			item = item.nextSibling as HTMLElement;
-		}
-		clicked.classList.add("magenta");
-	}
-
+	const pages = ['/', '/search', '/friends', '/chat', '/game'];
+	const location = useLocation();
+	const [page, setPage] = useState<string>();
+		
+	useEffect(() => {
+		const path = location.pathname;
+		for (let i in pages) {
+			path.startsWith(pages[i]) && setPage(pages[i])
+		}		
+	}, [location])
+	
 	return (
 		<div className="container-fluid">
 			<div className="row">
 				
-				<div className="col-sm vh-100 overflow-auto" style={{padding: "0"}}>  {/* min-vh-100 */}
+				<div className="col-sm vh-100 overflow-auto p-0" >
 					<Outlet />
 				</div>
-				<div className="col-sm-auto sticky-bottom order-sm-first" style={{backgroundColor: "black"}}>
+				<div className="col-sm-auto sticky-bottom order-sm-first bg-black">
 					<ul className="nav navbar navbar-expand flex-sm-column justify-content-around gap-sm-4" 
 						id="sidebar-ul" role="navigation" >
-						<li className='nav-item' onClick={handleClick}>
+						<li className={`nav-item ${(page == '/') && 'magenta'}`} >
 							<Link to="/"><img src={Home} /></Link>
 						</li>
-						<li className="nav-item" onClick={handleClick}>
+						<li className={`nav-item ${(page == '/search') && 'magenta'}`} >
 							<Link to="search"><img src={Search} /></Link>
 						</li>
-						<li className="nav-item" onClick={handleClick}>
+						<li className={`nav-item ${(page == '/friends') && 'magenta'}`} >
 							<Link to="friends"><img src={Friend} /></Link>
 						</li>
-						<li className="nav-item" onClick={handleClick}>
+						<li className={`nav-item ${(page == '/chat') && 'magenta'}`} >
 							<Link to="chat"><img src={Chat} /></Link>
 						</li>
-						<li className="nav-item" onClick={handleClick}>
+						<li className={`nav-item ${(page == '/game') && 'magenta'}`} >
 							<Link to="game"><img src={Game} /></Link>
 						</li>
 					</ul>
 				</div>
-
 			</div>
 		</div>
 	)
