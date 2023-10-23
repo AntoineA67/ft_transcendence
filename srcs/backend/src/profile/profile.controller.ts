0b015a76077ce8b0f124aba2ebb127d21c6ceb7c
Hsin Yu Liu <dewseileaf@gmail.com>
2023-10-23 11:35:08 +0200

    wip

---
+++ srcs/backend/src/profile/profile.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Req, UseGuards, Param } from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Request } from 'express';
+
+@Controller('profile')
+export class ProfileController {
+	constructor(private readonly profileService: ProfileService) { }
+
+	@UseGuards(JwtAuthGuard)
+	@Get('me')
+	async getMyProfile(@Req() req: Request) {
+		const id = req.user.id;
+		const profile = await this.profileService.getUserProfileById(id, id)
+		if (!profile) {
+			return ({ error: `user ${nick} not found` })
+		}
+		return (profile);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get(':nick')
+	async getProfile(@Req() req: Request, @Param('nick') nick: string) {
+		const id = req.user.id;
+		const profile = await this.profileService.getUserProfileByNick(id, nick);
+		if (!profile) {
+			return ({ error: `user ${nick} not found` })
+		}
+		return (profile);
+	}
+
+	
+
+}
