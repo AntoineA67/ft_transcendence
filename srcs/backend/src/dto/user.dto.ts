b3227ff87c93e6baa1cae9c0274c6f825bbb454b
Noushhhh <ana.andric@live.fr>
2023-10-25 18:03:26 +0200

    fix (get userid with refresh token) + updated UserDto with class validator pipes

--- srcs/backend/src/dto/user.dto.ts
+++ srcs/backend/src/dto/user.dto.ts
@@ -1,7 +1,18 @@
+import {    
+    IsNotEmpty, 
+    IsString, 
+    IsEmail,
+	IsNumber,
+ } from "class-validator";
+
 export class UserDto {
-	id: number;
-	username: string;
-	avatar: Buffer | null | ArrayBuffer;
-	status: 'ONLINE' | 'INGAME' | 'OFFLINE';
-	activated2FA?: boolean;
+	@IsNumber()								id?: number;
+	@IsNotEmpty() @IsString() @IsEmail() 	email?: string;
+	@IsNotEmpty() @IsString()				username?: string;
+	@IsNotEmpty() @IsString() 				password?: string;
+	@IsString() 							bio?: string;
+											avatar: Buffer | null | ArrayBuffer;
+											status: 'ONLINE' | 'OFFLINE' | 'INGAME';
+											otpHash?: string;
+											activated2FA?: boolean;
 }
