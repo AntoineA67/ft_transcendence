44e804332f6ead4141a4b97f2ac2e1a32b754da4
Antoine Adamy <antoine.adamy@gmail.com>
2023-10-26 16:20:06 +0200

    ahah

--- srcs/backend/src/main.ts
+++ srcs/backend/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthIoAdapter } from './AuthIoAdapter';
 import { AuthService } from './auth/auth.service';
+// require('newrelic');
+import 'newrelic';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
