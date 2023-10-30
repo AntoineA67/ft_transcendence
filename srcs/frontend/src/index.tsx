1702ad872748eda5a0732d35477d701b4cd18cc5
Noushhhh <ana.andric@live.fr>
2023-10-30 15:51:06 +0100

    merge with maser

--- srcs/frontend/src/index.tsx
+++ srcs/frontend/src/index.tsx
@@ -48,7 +48,7 @@ axios.defaults.baseURL = 'http://127.0.0.1:4000';
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
 
 async function loader(route: string, param?: string, refresh = false) {
-	const baseUrl = 'http://127.0.0.1:3000/';
+	const baseUrl = 'http://127.0.0.1:4000/';
 	const token = localStorage.getItem('token') || null;
 	const refreshToken = localStorage.getItem('refreshToken') || null;
 	const fetchUrl = param ? (`${baseUrl}${route}/${param}`) : (`${baseUrl}${route}`);
