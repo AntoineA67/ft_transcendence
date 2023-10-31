ff70aa0bf53d3cb2dbbf4e5cb0cf69ecf36d1c0d
Antoine Adamy <antoine.adamy@gmail.com>
2023-10-31 15:29:10 +0100

    Snackbar on 1mb file limit

--- srcs/frontend/src/pages/Profile.tsx
+++ srcs/frontend/src/pages/Profile.tsx
@@ -5,6 +5,7 @@ import { socket } from '../utils/socket';
 import { Avatar } from '../utils/Avatar';
 import { profileType } from '../../types/user';
 import { validatePassword, validateUsername } from "../utils/CredentialsRegex";
+import { closeSnackbar, enqueueSnackbar, SnackbarProvider } from "notistack";
 
 
 type textProp = {
@@ -42,20 +43,20 @@ type editTextProp = {
 
 function EditText({ type, profile, setProfile, setEdit }: editTextProp) {
 	const [mod, setMod] = useState(type == 'nick' ? profile.username : profile.bio);
-	
+
 	useEffect(() => {
 		const el = document.getElementsByTagName('textarea')[0] as HTMLTextAreaElement;
 		el.style.height = (el.scrollHeight) + "px";
 	}, [mod]);
-	
+
 	useEffect(() => {
 		const el = document.getElementsByTagName('textarea')[0] as HTMLTextAreaElement;
 		el.setSelectionRange(el.value.length, el.value.length);
 	}, []);
 
 	async function handleSubmit(
-		e: React.FormEvent<HTMLFormElement>, 
-		type: string, profile: profileType, 
+		e: React.FormEvent<HTMLFormElement>,
+		type: string, profile: profileType,
 		setProfile: React.Dispatch<React.SetStateAction<profileType>>
 	) {
 		const content = (type == 'nick') ? profile.username : profile.bio;
@@ -63,23 +64,23 @@ function EditText({ type, profile, setProfile, setEdit }: editTextProp) {
 		e.preventDefault();
 		if (mod == content) {
 			setEdit('done');
-			return ;
+			return;
 		}
-		let data = (type == 'nick') ? {username: mod} : {bio: mod};
+		let data = (type == 'nick') ? { username: mod } : { bio: mod };
 		socket.emit('UpdateProfile', data, (success: boolean) => {
 			console.log('success: ', success)
 			success && setProfile((prev) => (
-				prev ? ({...prev, ...obj}) : prev
+				prev ? ({ ...prev, ...obj }) : prev
 			));
 		})
 		setEdit('done');
 	}
 
 	return (
-		<form className="my-3 w-50 d-flex flex-column align-items-center" 
+		<form className="my-3 w-50 d-flex flex-column align-items-center"
 			onSubmit={(e) => handleSubmit(e, type, profile, setProfile)}>
 			<label htmlFor={`edit-${type}`}></label>
-			<textarea id={`edit-${type}`} className="w-100 text-center my-1 edit-text-area" 
+			<textarea id={`edit-${type}`} className="w-100 text-center my-1 edit-text-area"
 				autoFocus value={mod} onChange={(e) => setMod(e.target.value)}></textarea>
 			<button type="submit" className="ok" />
 		</form>
@@ -94,30 +95,33 @@ function NewAvatar({ setProfile }: NewAvatarProp) {
 	async function autoUpload() {
 		const input = document.getElementById('new-avatar') as HTMLInputElement;
 		const file = input.files ? input.files[0] : null;
-		if (!file) return ;
+		if (!file) return;
 		if (file.size >= 1048576) {
 			console.log('file size limit: 1MB');
-			return ;
+			enqueueSnackbar("Oups, la limite est d'1 MB !", {
+				variant: 'error',
+			})
+			return;
 		}
 		socket.emit('newAvatar', file, (success: boolean) => {
 			if (!success) {
 				console.log('fails');
-				return ;
+				return;
 			}
-			socket .emit('myAvatar', (avatar: string) => {
-				setProfile((prev) => ({ ...prev, avatar}))
+			socket.emit('myAvatar', (avatar: string) => {
+				setProfile((prev) => ({ ...prev, avatar }))
 			})
 		})
 	}
 
 	return (
 		<form id='form-avatar' action='link' method="post" encType="multipart/form-data">
-			<label htmlFor="new-avatar" className='add new-avatar-pos'>
-				<input 
-					className='d-none' 
-					id="new-avatar" 
-					type="file" 
-					name="new-avatar" 
+			<label htmlFor="new-avatar" className='add new-avatar-pos' title="Max 1Mb">
+				<input
+					className='d-none'
+					id="new-avatar"
+					type="file"
+					name="new-avatar"
 					// accept="image/*"
 					accept=".png, .jpg, .jpeg"
 					onChange={autoUpload}
@@ -133,32 +137,33 @@ function Profile() {
 
 	return (
 		<>
-			<div className="container my-5 pb-sm-5 d-flex flex-column align-items-center white-text">			
+			<SnackbarProvider maxSnack={3} />
+			<div className="container my-5 pb-sm-5 d-flex flex-column align-items-center white-text">
 				<Link to="/setting"><button className="setting m-3 position-absolute top-0 end-0" /></Link>
-			
+
 				<div>
 					<Avatar size={150} user={{
-						id: profile.id, 
-						username: profile.username, 
+						id: profile.id,
+						username: profile.username,
 						avatar: profile.avatar,
 						status: profile.status
 					}} />
 				</div>
-				<NewAvatar setProfile={setProfile}/>
+				<NewAvatar setProfile={setProfile} />
 
-				{ (edit == 'nick'
-					) ? ( 
-						<EditText type='nick' profile={profile} setProfile={setProfile} setEdit={setEdit} /> 
-					) : (
-						<Text type='nick' profile={profile} setEdit={setEdit} /> )}
-				
-					{ (edit == 'bio'
-					) ? (
-						<EditText type='bio' profile={profile} setProfile={setProfile} setEdit={setEdit} />
-					) : (
-						<Text type='bio' profile={profile} setEdit={setEdit} />)}
+				{(edit == 'nick'
+				) ? (
+					<EditText type='nick' profile={profile} setProfile={setProfile} setEdit={setEdit} />
+				) : (
+					<Text type='nick' profile={profile} setEdit={setEdit} />)}
+
+				{(edit == 'bio'
+				) ? (
+					<EditText type='bio' profile={profile} setProfile={setProfile} setEdit={setEdit} />
+				) : (
+					<Text type='bio' profile={profile} setEdit={setEdit} />)}
 			</div>
-			<Stat gameHistory={profile.gameHistory.map((a) => ({...a}))} achieve={{...(profile.achieve)}} />
+			<Stat gameHistory={profile.gameHistory.map((a) => ({ ...a }))} achieve={{ ...(profile.achieve) }} />
 		</>
 	);
 }
