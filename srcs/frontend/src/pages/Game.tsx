2676b10bd6ea55b0406f608a336c07035474471a
Antoine Adamy <antoine.adamy@gmail.com>
2023-11-04 15:45:01 +0100

    pong button working on profiles

--- srcs/frontend/src/pages/Game.tsx
+++ srcs/frontend/src/pages/Game.tsx
@@ -189,8 +189,8 @@ function RulesPopup() {
 				<Modal.Header closeButton>
 					<Modal.Title className="text-black" >Rules</Modal.Title>
 				</Modal.Header>
-				<Modal.Body className="text-black" >Pong is one of the first computer games ever created. This simple "tennis like" game features two paddles and a ball, the goal is to defeat your opponent by being the first one to gain 10 points, a player gets a point once the opponent misses a ball. The game can be played with two human players, or one player against a computer controlled paddle.
-					Use the up and down arrows to move your paddle. You can also play with your hands, using your computer camera.</Modal.Body>
+				<Modal.Body className="text-black" >Pong is one of the first computer games ever created. This simple "tennis like" game features two paddles and a ball, the goal is to defeat your opponent by being the first one to gain 5 points, a player gets a point once the opponent misses a ball. The game can be played with two human players, or one player against a computer controlled paddle.
+					Use the up and down arrows or W and S to move your paddle. You can also play with your hands, using your computer camera.</Modal.Body>
 				<Modal.Footer>
 					<button className="btn btn-primary" onClick={handleClose}>
 						Close
