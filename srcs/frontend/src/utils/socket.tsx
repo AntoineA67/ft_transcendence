5d6c7374eda2965e751f6d42a2412b230770e77f
Florian <git@florian-a.dev>
2023-11-07 03:23:08 +0100

    Rhoooooooo ça marche, enfin il y a un petit bug mais bon, dans la vie on ne réussie pas tout du premier coup !

--- srcs/frontend/src/utils/socket.tsx
+++ srcs/frontend/src/utils/socket.tsx
@@ -1,13 +1,12 @@
 import { io } from "socket.io-client";
 
-//const URL: string = process.env.REACT_APP_BACKEND_URL?.toString() || '';
+const URL: string = process.env.REACT_APP_FRONTEND_URL?.toString() || '';
 
 const socketOptions = {
 	autoConnect: false,
 	transports: ['websocket'],
 };
 
-const URL: string = 'http://localhost';
 export const socket = io(URL, socketOptions);
 export const friendsSocket = io(`${URL}/friends`, socketOptions);
 export const chatsSocket = io(`${URL}/chats`, socketOptions);
