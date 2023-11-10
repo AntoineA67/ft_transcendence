53cc3526224c280309cad0441c8d48fab140e363
Florian <git@florian-a.dev>
2023-11-10 13:45:21 +0100

    New deploy !

--- docs/do-init.mk
+++ docs/do-init.mk
@@ -11,7 +11,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin make
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin make -y
 
 ssh-keygen -t ed25519 -C "deploy@github.com"
 
