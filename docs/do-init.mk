80529c815a04666eb7d91eb815bbffc0e30b3f7e
Florian <git@florian-a.dev>
2023-11-10 04:02:02 +0100

    EOUEU

--- docs/do-init.mk
+++ docs/do-init.mk
@@ -13,4 +13,6 @@ apt update
 
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin make
 
-ssh-keygen -t ed25519 -C "deploy@github.com
+ssh-keygen -t ed25519 -C "deploy@github.com"
+
+ssh-keygen -F github.com || ssh-keyscan github.com >> ~/.ssh/known_hosts
