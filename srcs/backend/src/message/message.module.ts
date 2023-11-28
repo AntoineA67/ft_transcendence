f6119e91d78f71360d4034a4cf7bfe6ecfc76668
Hsin Yu Liu <dewseileaf@gmail.com>
2023-11-28 19:47:01 +0100

    remove member and message controller

--- srcs/backend/src/message/message.module.ts
+++ srcs/backend/src/message/message.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import MessagesController from './messages.controller';
 import { MessagesService } from './messages.service';
 import { MessageGateway } from './message.gateway';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [MessagesController],
+  controllers: [],
   providers: [MessagesService, MessageGateway],
 })
 export class MessagesModule { }
