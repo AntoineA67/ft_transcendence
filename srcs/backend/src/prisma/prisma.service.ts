a62b8e190dc12c3c569f7764981bbec8ab8e8586
Hsin Yu Liu <dewseileaf@gmail.com>
2023-11-28 23:49:18 +0100

    comment out new PrismaClient

--- srcs/backend/src/prisma/prisma.service.ts
+++ srcs/backend/src/prisma/prisma.service.ts
@@ -2,7 +2,8 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 // ???
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
+// I insist to delete the line
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
