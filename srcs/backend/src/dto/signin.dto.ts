c078cebb1f9cf54d2e1a62199d9fb9ee8a9aa808
Noushhhh <ana.andric@live.fr>
2023-12-04 20:24:41 +0100

    fix (dtos secured length + catch all exceptions auth controller)

--- srcs/backend/src/dto/signin.dto.ts
+++ srcs/backend/src/dto/signin.dto.ts
@@ -3,11 +3,18 @@ import {
     IsNotEmpty, 
     IsString, 
     IsEmail,
+    MaxLength,
+    MinLength,
  } from "class-validator";
 
 // The DTO (Data Transfer Object) for authentication operations
 export class SigninDto {
-    @IsNotEmpty() @IsString() @IsEmail() email!: string;
-    @IsNotEmpty() @IsString()  password!: string;
+    @IsNotEmpty() @IsString() @IsEmail()
+    @MaxLength(100) @MinLength(3) 
+    email!: string;
+
+    @IsNotEmpty() @IsString() 
+    @MaxLength(20) @MinLength(8) 
+    password!: string;
     @IsString()  token2FA?: string;
 }
